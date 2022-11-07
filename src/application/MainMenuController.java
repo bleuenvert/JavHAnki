1262020660edfaf4fe648de0f09a3f58bad0d7f5
bleuenvert <e.j.flynn96@gmail.com>
2022-11-06 21:25:09 -0700

    Added MainMenuController
    Built basic skeleton of the class. Currently only included the
    TitledPane and Buttons since they are the only elements I plan on
    interacting in its current form.

---
+++ src/application/MainMenuController.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TitledPane;
+
+public class MainMenuController {
+	
+	@FXML
+	private TitledPane mainMenuTitledPane;
+	
+	@FXML
+	private Button studyMenuButton;
+	
+	@FXML
+	private Button deckManagementButton;
+	
+	@FXML
+	private Button viewDataButton;
+	
+	
+
+}

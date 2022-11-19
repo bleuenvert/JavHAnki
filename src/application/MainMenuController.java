bf0f2381412eb7fde4fbd720787858e4cf85e01e
bleuenvert <e.j.flynn96@gmail.com>
2022-11-18 21:16:52 -0700

    Added study menu controller and study menu fxml file.
    Not currently in working shape. I have fixed a few errors that arose but
    think I am still making errors in how I am creating the study menu.

--- src/application/MainMenuController.java
+++ src/application/MainMenuController.java
@@ -1,16 +1,22 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+import java.io.FileInputStream;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class MainMenuController {
+	Stage applicationStage;
 	
 	@FXML
 	private TitledPane mainMenuTitledPane;
-	
-	@FXML
-	private Button studyMenuButton;
 	
 	@FXML
 	private Button deckManagementButton;
@@ -18,6 +24,25 @@ public class MainMenuController {
 	@FXML
 	private Button viewDataButton;
 	
+	@FXML
+	private Button studyMenuButton;
+	
+	
+	@FXML
+	void openStudyMenu(ActionEvent openStudyMenuEvent) {
+		Scene mainScene = applicationStage.getScene();
+			try {
+				FXMLLoader loader = new FXMLLoader();
+				VBox root = loader.load(new FileInputStream("src/application/StudyMenuView.fxml"));
+				Scene scene = new Scene(root,400,400);
+				applicationStage.setScene(scene);
+				applicationStage.setTitle("Study Menu");
+				applicationStage.show();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	
 
-}

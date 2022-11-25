1c2346b865da89ddc437e2344ddffd6b04e47b1a
bleuenvert <e.j.flynn96@gmail.com>
2022-11-24 21:42:26 -0700

    Updated DeckMenuView.fxml
    Added some modules to the file and updated the controller with the
    respective modules. No functionality yet just the skeleton.

--- src/application/DeckMenuController.java
+++ src/application/DeckMenuController.java
@@ -8,13 +8,43 @@ import java.io.FileInputStream;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class DeckMenuController {
 	Stage applicationStage;
+	
 	@FXML 
 	private Label titleLabel;
+	
+	@FXML
+	private TextField deckNameTextfield;
+	
+	@FXML
+	private Button addDeckButton;
+	
+	@FXML
+	private ChoiceBox<String> deckListChoiceBox;
+	
+	@FXML
+	private Button editDeckButton;
+	
+	
+	
+	@FXML
+	void addDeck(ActionEvent addDeck) {
+		
+	}
+	
+	
+	@FXML
+	void editDeck(ActionEvent editDeck) {
+	
+	}
+	
+	
 }

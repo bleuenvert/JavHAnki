82baf73727c05847e55ff12c30fa73c109645c55
bleuenvert <e.j.flynn96@gmail.com>
2022-12-07 10:38:49 -0700

    Added data file for each deck as each deck is created.
    renamed editdeck button to refresh button to refresh the choicebox deck
    list

--- src/application/DeckMenuController.java
+++ src/application/DeckMenuController.java
@@ -38,7 +38,7 @@ public class DeckMenuController {
 
 	
 	@FXML
-	private Button editDeckButton;
+	private Button refreshDeckButton;
 	
 	
 	
@@ -49,26 +49,40 @@ public class DeckMenuController {
 		//https://docs.oracle.com/javase/7/docs/api/java/nio/file/Path.html
 		//https://stackoverflow.com/questions/4871051/how-to-get-the-current-working-directory-in-java
 		// I am not sure if this works on windows. I might need to replace "/" with a system dependent separator
-		Path path = FileSystems.getDefault().getPath("src", "Decks");
-		String newDeckPath = path.toString() + "/" + newDeck.getDeckName() + ".txt";
+		Path deckPath = FileSystems.getDefault().getPath("src", "Decks");
+		Path dataPath = FileSystems.getDefault().getPath("src", "Data");
+		String newDeckPath = deckPath.toString() + "/" + newDeck.getDeckName() + ".txt";
+		String newDeckDataPath = dataPath.toString() + "/" + newDeck.getDeckName() + ".data"; 
 		
 		File deckFile = new File(newDeckPath);
+		File deckDataFile = new File(newDeckDataPath);
 		//https://www.w3schools.com/java/java_files_create.asp was helpful
 		try {
 		
 		if (deckFile.createNewFile()) {
 			System.out.println(deckFile.getAbsolutePath());
 		} else {
-			System.out.println("File already exists");
+			System.out.println("Deck with that name already exists");
 		}
 		} catch (IOException e){
-			System.out.println("Error occured");
+			System.out.println("Error occured when creating Deck");
 			e.printStackTrace();
 	}
+		try {
+			
+			if (deckDataFile.createNewFile()) {
+				System.out.println(deckDataFile.getAbsolutePath());
+			} else {
+				System.out.println("Deck data file with that name already exists");
+			}
+			} catch (IOException e){
+				System.out.println("Error occured when creating Deck Data file");
+				e.printStackTrace();
+		}
 	}
 	
 	@FXML
-	void editDeck(ActionEvent editDeck) {
+	void refeshDeckOptions(ActionEvent editDeck) {
 		Path path = FileSystems.getDefault().getPath("src", "Decks");
 		File Decks = new File(path.toString());
 		String DeckContents[] = Decks.list();
